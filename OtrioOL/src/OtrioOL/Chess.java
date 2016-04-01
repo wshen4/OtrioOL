9728378814d8dc702097236b9181a4aa34865db0
sqpan <ShaoqingPan@192.168.0.5>
2016-04-01 17:27:03 -0400

    chess class

--- OtrioOL/src/OtrioOL/Chess.java
+++ OtrioOL/src/OtrioOL/Chess.java
@@ -3,13 +3,18 @@ package OtrioOL;
 public class Chess {
 	
 	//member variable
-	public enum chessType{
+	private enum Type{
 		small, medium, large
 	}
+	private enum Color{
+		blue, red, green, purple
+	}
+	private Type chessType;
+	private Color chessColor;
 	
-	
-	
-	
-	
-
+	//constructor
+	public Chess(Type chessTpye, Color chessColor){
+		this.chessType = chessType;
+		this.chessColor = chessColor;
+	}
 }
