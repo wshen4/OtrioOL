81521eef8872862874577ec98a6ca01561f77978
sqpan <ShaoqingPan@192.168.0.10>
2016-04-07 18:10:27 -0400

    check win version 1

--- OtrioOL/src/OtrioOL/Chessboard.java
+++ OtrioOL/src/OtrioOL/Chessboard.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 
 public class Chessboard {
 	private ArrayList<ArrayList<Integer>> board;
+	private ArrayList<ArrayList<Integer>> threeWay;
 	public Chessboard(){
 		board = new ArrayList<ArrayList<Integer>>();
 		for(int i = 0; i < 9; i++){
@@ -13,6 +14,17 @@ public class Chessboard {
 				tmp.add(0);
 			}
 			board.add(tmp);
+		}
+		//build an new ArrayList to save three possible position in a roll 
+		threeWay = new ArrayList<ArrayList<Integer>>();
+		//all possible combination of three position in a roll, every three number is a set
+		int a[] = {0,1,2,0,3,6,6,7,8,2,5,8,1,4,7,3,4,5,0,4,8,2,4,6};
+		for (int i = 0; i < 8; i++) {
+			ArrayList array1 = new ArrayList();
+			for (int j = 0; j < 3; j ++) {
+				array1.add(a[i * 3 + j]);
+			}
+			threeWay.add(array1);
 		}
 	}
 
@@ -29,42 +41,63 @@ public class Chessboard {
 		return board.get(input);
 	}
 	
-	//check condition wrap up win condition
 	private boolean checkWin(Player player){
-		int currentPlayer = player.getId();
-		//1.check for player 1		
+		//record current player
+		int currentPlayer = player.getId();		
 		//	(1).check if the three chess in one position
-		if (currentPlayer == 1) {
-			for (int i = 0; i < 9; i++){
-				int sum = board.get(i).get(0) + board.get(i).get(1) + board.get(i).get(2);
-				if (sum == 3) {
-					return true;
-				}
-			}//end for
-		}//end if
-		
-		//check for player 2
-		//	(1).check if the three chess in one position
-		else {
-			for (int i = 0; i < 9; i++){
-				int sum = board.get(i).get(0) + board.get(i).get(1) + board.get(i).get(2);
-				if (sum == 6) {
-					return true;
-				}
-			}//end for
-		}//end if
-		//	(2). check if t
-		return false;	
-	}
-	//win condition warp up
-	//win condition one: three in the roll
-		//1.check if the three position in the roll
-			//get sum of the all nine points in same row of column
-			//player 1 qualify: %3 = 0
-			//player 2 qualify: 
-		//2.check if the three chess in certain pattern:small-medium-big, 
-		//	b-m-s, s-s-s, m-m-m, b-b-b
-	//win condition two: occupy all three in one position: big-medium-small
-	
+		for (int i = 0; i < 9; i++){
+			int sum = board.get(i).get(0) + board.get(i).get(1) + board.get(i).get(2);
+			if (sum == currentPlayer * 3) {					
+				return true;
+			}
+		}//end for
+		//	(2). check if three in the roll
+		// create a arraylist to save the sequence if there are three position in the three
+		ArrayList<Integer> threeInRoll = new ArrayList<Integer>();
+		for (int i = 0; i < 8; i++) {
+			ArrayList<Integer> tmp = new ArrayList<Integer>();
+			int sum[] = {};
+			for (int j = 0; j < 3; j++) {
+				tmp.add(threeWay.get(i).get(j));
+				sum[j] = (board.get(tmp.get(j)).contains(currentPlayer)) ? 1 :0 ;
+			}
+			if (sum[0] + sum[1] + sum[2] == 1) {
+				threeInRoll.addAll(tmp);
+				continue;
+			}
+			else {
+				return false;
+			}
+		}
+		// (3). check if all three chess are in right order: small-med-big; b-m-s; s-s-s; m-m-m; b-b-b
+		// s-m-b
+		if (board.get(threeInRoll.get(0)).get(0) == 0 && board.get(threeInRoll.get(1)).get(1) == 1 && 
+			board.get(threeInRoll.get(2)).get(2) == 2) {
+			return true;
+		}
+		//b-m-s
+		else if (board.get(threeInRoll.get(0)).get(0) == 2 && board.get(threeInRoll.get(1)).get(1) == 1 && 
+			board.get(threeInRoll.get(2)).get(2) == 0) {
+			return true;
+		}
+		//b-b-b
+		else if (board.get(threeInRoll.get(0)).get(0) == 2 && board.get(threeInRoll.get(1)).get(1) == 2 && 
+				board.get(threeInRoll.get(2)).get(2) == 2) {
+				return true;
+		}
+		//m-m-m
+		else if (board.get(threeInRoll.get(0)).get(0) == 1 && board.get(threeInRoll.get(1)).get(1) == 1 && 
+				board.get(threeInRoll.get(2)).get(2) == 1) {
+				return true;
+		}
+		//s-s-s
+		else if (board.get(threeInRoll.get(0)).get(0) == 0 && board.get(threeInRoll.get(1)).get(1) == 0 && 
+				board.get(threeInRoll.get(2)).get(2) == 0) {
+				return true;
+		}
+		//not in order return false
+		else 
+			return false;	
+	}	
 }
 
